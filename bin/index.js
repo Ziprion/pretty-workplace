860a7213683ad8ad705b5830d9f58713d3c3c069
Maxim Novikov <Maxim_Novikov@epam.com>
2021-12-05 13:41:47 +0300

    fix server port

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,6 @@ import { runServer } from '../server/index.js';
 
 const mode = process.env.NODE_ENV;
 const isDev = mode === 'development';
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT || process.env.SERVER_PORT;
 
 runServer({ isDev }).listen(port);
